c4d3ce3f2cd3936b6c4a48402fcaa23bdbfae703
Khris Punzalan <khrispunzalan@gmail.com>
2019-06-22 00:37:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
+    # binding.pry
+    self.split(/[.?!]/).count
 
+    # sentence_count = self.split(/\.|\?|!/).length
+    # puts sentence_count
+
+    #string.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+
+    # self.split(".", "?", "!", "!!", "...").count
+    # .length?
   end
+
 end
