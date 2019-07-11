f29323a2883c37700ec2c66021a4a285b18087f5
Aida Abdikulova <abdikulova@gmail.com>
2019-07-11 03:20:04 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,20 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence?()
+    self.end_with?(".")
   end
 
-  def question?
-
+  def question?()
+    self.end_with?("?")
   end
 
-  def exclamation?
-
+  def exclamation?()
+    self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences()
+    self.split(/[.!?]+/).count
   end
+
 end
