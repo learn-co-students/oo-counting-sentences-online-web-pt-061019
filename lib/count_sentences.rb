c68b469adc76c552706ed1d59a3a881551f7a6f3
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-22 18:32:21 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+   self.end_with?("!")
   end
 
   def count_sentences
-
+    word_splits = self.split(/\?|\.|\!/)
+    #binding.pry
+    sentance_splits = []
+    word_splits.each do |a|
+    if a != ""
+    sentance_splits << a
+    end
+    end
+    sentance_splits.count 
   end
 end
